a32204f06e978d3fd8184ae66b21c82fbbb3a005
Émile Perron <contact@emileperron.com>
2020-09-26 20:00:26 -0400

    Added about tab and small UI improvements with tooltips

---
+++ src/popup/tab-plans.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*
+ * /popup/tab-plans.js
+ * ...
+ */
+
+window.addEventListener("message", (event) => {
+    if (!event.data || event.data.type != 'shiftpic') {
+        return;
+    }
+
+    if (event.data.action == 'fastSpringPopupClosed') {
+        document.querySelector('iframe[src^="https://www.shiftpic.co/checkout"]').remove();
+
+        if (event.data.id) {
+            Flash.show('success', chrome.i18n.getMessage('tab_plans_new_order_validating'));
+            chrome.storage.local.set({ 'orderId': event.data.id });
+
+            License.saveLicenseFromOrder(event.data.id, () => {
+                License.get((license) => {
+                    if (license) {
+                        License.getType((type) => {
+                            Flash.show('success', chrome.i18n.getMessage('tab_plans_new_order_success').replace('%s', type));
+                        });
+                    }
+                });
+            });
+        }
+    }
+}, false);
+
+Dom.delegate('click', '[checkout-trigger]', function(e) {
+    e.preventDefault();
+    showSubscriptionPlanCheckout(this.getAttribute('checkout-trigger'));
+});
+
+function showSubscriptionPlanCheckout(plan) {
+    // Remove any existing checkout frame
+    const existingIframe = document.querySelector('iframe[src^="https://www.shiftpic.co/checkout"]');
+    if (existingIframe) {
+        existingIframe.remove();
+    }
+
+    const newIframe = document.createElement('iframe');
+    newIframe.src = "https://www.shiftpic.co/checkout?" + plan;
+    newIframe.setAttribute('frameborder', 0);
+    newIframe.classList.add('checkout-popup');
+    document.body.appendChild(newIframe);
+}

ec59b2cb13d8880b2dfcfc0690c86a6de8308c70
Émile Perron <contact@emileperron.com>
2020-09-26 20:34:25 -0400

    Implemented manual license change in the User tab

--- src/popup/tab-user.js
+++ src/popup/tab-user.js
@@ -4,3 +4,27 @@
  * /popup/tab-user.js
  * ...
  */
+
+document.querySelector('#tab-user').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    License.get((license) => {
+        let formData = new FormData(this);
+        formData.append('currentLicense', license);
+
+        fetch("https://api.shiftpic.co/license/validateChange", { method: 'POST', body: formData }).then((response) => {
+            return response.json();
+        }).then((response) => {
+            if (!response) {
+                Flash.show('error', 'An unknown error occured. Please close this window and try again.');
+            } else if (response.error) {
+                const message = chrome.i18n.getMessage('error_' + response.error);
+                Flash.show('error', message ? message : response.error);
+            } else if (response.license && response.type) {
+                License.set(response.license, () => {
+                    Flash.show('success', chrome.i18n.getMessage('tab_user_license_change_success'));
+                });
+            }
+        });
+    });
+});
